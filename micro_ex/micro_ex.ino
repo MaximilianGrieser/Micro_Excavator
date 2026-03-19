71bad38dcae60e40a733d0b1e54e5ccc8d0f6633
MaximilianGrieser <61865913+MaximilianGrieser@users.noreply.github.com>
2026-03-19 10:13:58 +0100

    add Stepper controlls

--- micro_ex/micro_ex.ino
+++ micro_ex/micro_ex.ino
@@ -1,4 +1,11 @@
-#define ESTOP 2
+
+#define ESTOP 1
+#define ROTATOR_STEP 7
+#define ROTATOR_DIR 6
+#define TILT_STEP 5
+#define TILT_DIR 4
+#define CLAW_STEP 3
+#define CLAW_DIR 2
 #define BOTTOMRIGHTAXISX A0
 #define BOTTOMRIGHTAXISY A1
 #define UPPERRIGHTAXISX A2
@@ -10,10 +17,20 @@
 
 volatile unsigned long lastInterrupt = 0;
 volatile bool eStop = false;
+volatile unsigned int stepDelay = 500;
+volatile unsigned int maxJoyValue = 1024;
+volatile unsigned int joyMiddle = maxJoyValue / 2;
+volatile unsigned int deadzone = 50;
 
 void setup() {
   pinMode(ESTOP, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ESTOP), isr_stop, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(ESTOP), isr_stop, FALLING);
+  pinMode(ROTATOR_STEP, OUTPUT);
+  pinMode(ROTATOR_DIR, OUTPUT);
+  pinMode(TILT_STEP, OUTPUT);
+  pinMode(TILT_DIR, OUTPUT);
+  pinMode(CLAW_STEP, OUTPUT);
+  pinMode(CLAW_DIR, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -27,7 +44,7 @@ void loop() {
     int bototmLeftY = analogRead(BOTTOMLEFTAXISY);
     int upperLeftX = analogRead(UPPERLEFTAXISX);
     int upperLeftY = analogRead(UPPERLEFTAXISY);
-    Serial.print("BRX-Axis: ");
+    /*Serial.print("BRX-Axis: ");
     Serial.print(bottomRightX);
     Serial.print(" | BRY-Axis: ");
     Serial.print(bottomRightY);
@@ -36,19 +53,63 @@ void loop() {
     Serial.print(" | URY-Axis: ");
     Serial.print(upperRightY);
     Serial.print(" | BLX-Axis: ");
-    Serial.print(bottomLeftX);
+    Serial.println(bottomLeftX);
     Serial.print(" | BLY-Axis: ");
-    Serial.print(bototmLeftY);
+    Serial.println(bototmLeftY);
     Serial.print(" | ULX-Axis: ");
     Serial.print(upperLeftX);
     Serial.print(" | ULY-Axis: ");
     Serial.print(upperLeftY);
-    Serial.println(" ");
+    Serial.println(" ");*/
+
+    if(bottomRightX > joyMiddle + deadzone) {
+      digitalWrite(ROTATOR_DIR, HIGH);
+      digitalWrite(ROTATOR_STEP, HIGH);
+      delayMicroseconds(stepDelay);
+      digitalWrite(ROTATOR_STEP, LOW);
+      delayMicroseconds(stepDelay);
+    }
+    if(bottomRightX < joyMiddle - deadzone) {
+      digitalWrite(ROTATOR_DIR, LOW);
+      digitalWrite(ROTATOR_STEP, HIGH);
+      delayMicroseconds(stepDelay);
+      digitalWrite(ROTATOR_STEP, LOW);
+      delayMicroseconds(stepDelay);
+    }
+
+    if(bottomRightY > joyMiddle + deadzone) {
+      digitalWrite(TILT_DIR, HIGH);
+      digitalWrite(TILT_STEP, HIGH);
+      delayMicroseconds(stepDelay);
+      digitalWrite(TILT_STEP, LOW);
+      delayMicroseconds(stepDelay);
+    }
+    if(bottomRightY < joyMiddle - deadzone) {
+      digitalWrite(TILT_DIR, LOW);
+      digitalWrite(TILT_STEP, HIGH);
+      delayMicroseconds(stepDelay);
+      digitalWrite(TILT_STEP, LOW);
+      delayMicroseconds(stepDelay);
+    }
+
+    if(bottomLeftX > joyMiddle + deadzone) {
+      digitalWrite(CLAW_DIR, HIGH);
+      digitalWrite(CLAW_STEP, HIGH);
+      delayMicroseconds(stepDelay);
+      digitalWrite(CLAW_STEP, LOW);
+      delayMicroseconds(stepDelay);
+    }
+    if(bottomLeftX < joyMiddle - deadzone) {
+      digitalWrite(CLAW_DIR, LOW);
+      digitalWrite(CLAW_STEP, HIGH);
+      delayMicroseconds(stepDelay);
+      digitalWrite(CLAW_STEP, LOW);
+      delayMicroseconds(stepDelay);
+    }
+
   } else {
     Serial.println("Interrupt");
   }
-
-  delay(100);
 }
 
 void isr_stop() {
